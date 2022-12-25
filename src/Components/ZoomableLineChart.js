4965ce1297a0af6c946830476a86d9c44a4b3a0d
VaggV <vag_vasiliou@hotmail.com>
2022-12-25 22:56:20 +0200

    Chart customizations

--- src/Components/ZoomableLineChart.js
+++ src/Components/ZoomableLineChart.js
@@ -3,7 +3,6 @@ import {
   select,
   scaleLinear,
   line,
-  max,
   axisBottom,
   axisLeft,
   zoom,
@@ -15,11 +14,13 @@ import useResizeObserver from "./useResizeObserver";
  * Component that renders a ZoomableLineChart
  */
 
-function ZoomableLineChart({ data, data2, dataX , id = "myZoomableLineChart" }) {
+function ZoomableLineChart({ data, data2, dataX, maxHeight, minHeight, id = "myZoomableLineChart" }) {
   const svgRef = useRef();
   const wrapperRef = useRef();
   const dimensions = useResizeObserver(wrapperRef);
   const [currentZoomState, setCurrentZoomState] = useState();
+
+  // console.log("rerender")
 
   // will be called initially and on every data change
   useEffect(() => {
@@ -27,17 +28,18 @@ function ZoomableLineChart({ data, data2, dataX , id = "myZoomableLineChart" }) 
 
     const svg = select(svgRef.current);
     const svgContent = svg.select(".content");
-    const { width, height } =
-      dimensions || wrapperRef.current.getBoundingClientRect();
+    const { width, height } = { width: 600, height: 600};
+      // dimensions || wrapperRef.current.getBoundingClientRect();
     
     // scales + line generator
     const xScale = scaleLinear()
-      .domain([0, data.length - 1])
+      .domain([0, data[data.length - 1].MeterDistance])
       .range([10, width - 10]);
 
     const yScale = scaleLinear()
-      .domain([0, max(data)])
+      .domain([minHeight, maxHeight])
       .range([height - 10, 10]);
+
 
     if (currentZoomState) {
       const newXScale = currentZoomState.rescaleX(xScale);
@@ -50,8 +52,8 @@ function ZoomableLineChart({ data, data2, dataX , id = "myZoomableLineChart" }) 
     
 
     const lineGenerator = line()
-      .x((d, index) => xScale(index))
-      .y((d) => yScale(d));
+      .x((d) => xScale(d.MeterDistance))
+      .y((d) => yScale(d.Height));
 
     const lineGenerator2 = line()
       .x((d, index) => xScale(dataX[index]))
@@ -94,11 +96,12 @@ function ZoomableLineChart({ data, data2, dataX , id = "myZoomableLineChart" }) 
       .attr("class", "myDot")
       .attr("stroke", "black")
       .attr("r", 4)
-      .attr("fill", "orange")
-      .attr("cx", (value, index) => xScale(index))
-      .attr("cy", yScale)
-      .attr("stroke-width", 30)
-      .attr("stroke-opacity", 0)
+      .attr("fill", (value) => (value.IsVisible ? "green" : "red")) 
+      .attr("cx", (value) => xScale(value.MeterDistance))
+      .attr("cy", (value) => yScale(value.Height))
+      // .attr("stroke-width", 30)
+      // .attr("stroke-opacity", 0)
+      .attr("content-visibility", "auto")
       // .attr("stroke-linejoin", "round")
 
     const circles = svgContent
@@ -108,7 +111,7 @@ function ZoomableLineChart({ data, data2, dataX , id = "myZoomableLineChart" }) 
     circles.on("mouseover", function (event, d) {
       // console.log("d", d)
       tip.style("opacity", 1)
-            .html(d)
+            .html(d.Height.toFixed(2))
             .style("left", (event.pageX-25) + "px")
             .style("top", (event.pageY-35) + "px")
     })
@@ -123,18 +126,36 @@ function ZoomableLineChart({ data, data2, dataX , id = "myZoomableLineChart" }) 
 
 
     // axes
-    const xAxis = axisBottom(xScale);
+    const xAxis = axisBottom(xScale)
+    .ticks(((width + 2) / (height + 2)) * 10)
+    .tickSize(-height)
+    .tickPadding(8)
+    // Make the first and last tick invisible
+    .tickSizeOuter(0)
+    // Make the space between each tick smaller
+
     svg
       .select(".x-axis")
       .attr("transform", `translate(0, ${height})`)
-      .call(xAxis);
+      .call(xAxis)
+      .attr("stroke-opacity", `0.1`)
+      .attr("shape-rendering", `crispEdges`);
 
-    const yAxis = axisLeft(yScale);
-    svg.select(".y-axis").call(yAxis);
+    const yAxis = axisLeft(yScale)
+    .ticks(10)
+    .tickSize(-width)
+    .tickPadding(8)
+    .tickSizeOuter(0)
+    
+    svg
+    .select(".y-axis")
+    .call(yAxis)
+    .attr("stroke-opacity", `0.1`)
+    .attr("shape-rendering", `crispEdges`);
 
     // zoom
     const zoomBehavior = zoom()
-      .scaleExtent([0.5, 10000])
+      .scaleExtent([0.5, 10000]) // Min zoom level, max zoom level, 1 is default, 0.5 is smaller
       .translateExtent([
         [0, 0],
         [width, height],
@@ -142,23 +163,24 @@ function ZoomableLineChart({ data, data2, dataX , id = "myZoomableLineChart" }) 
       .on("zoom", (event) => {
         const zoomState = event.transform;
         setCurrentZoomState(zoomState);
+        // console.log("zoomstate", zoomState.k)
       });
 
     svg.call(zoomBehavior);
 
-  }, [currentZoomState, data, dimensions]);
+  }, [currentZoomState, data, data2, dataX, dimensions, maxHeight, minHeight]);
 
   return (
     <React.Fragment>
-      <div className="tooltip" style={{ opacity: "0" }}></div>
+      <div className="tooltip"></div>
       <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
         <svg ref={svgRef}>
           <defs>
-            <clipPath id={id}>
+            <clipPath id="myChart">
               <rect x="0" y="0" width="100%" height="100%" />
             </clipPath>
           </defs>
-          <g className="content" clipPath={`url(#${id})`}></g>
+          <g className="content" clipPath={'url(#myChart)'}></g>
           <g className="x-axis" />
           <g className="y-axis" />
         </svg>
