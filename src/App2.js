2f77a36a1d4ee27a372724ce3d82a66af25d8758
VaggV <vag_vasiliou@hotmail.com>
2023-09-07 11:19:33 +0300

    Test signature pad library

--- src/App2.js
+++ src/App2.js
@@ -1,14 +1,18 @@
 // import { VictoryChart, VictoryZoomContainer, VictoryScatter } from 'victory';
 import React from "react";
-import ImageEditorTest from "./Components/ImageEditorTest";
-import CapacitorTest from "./Components/CapacitorTests";
+// import ImageEditorTest from "./Components/ImageEditorTest";
+// import CapacitorTest from "./Components/CapacitorTests";
+import SignaturePad from 'react-signature-pad-wrapper'
 
 function App2() {
 
     return (
         <div style={{ margin: 0 }}>
-            <ImageEditorTest />
-            <CapacitorTest  />
+
+            {/* <ImageEditorTest /> */}
+            {/* <CapacitorTest  /> */}
+           <SignaturePad width={500} height={500} />;
+
         </div>
       );
 }
